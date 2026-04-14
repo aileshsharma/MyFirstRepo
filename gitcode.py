3774cf8747a60f88b5be349136efb9d1dee50580
aileshsharma <sharmaailesh7@gmail.com>
2026-04-15 00:43:12 +0530

    Added library application

---
+++ gitcode.py
@@ -0,0 +1,25 @@
+# Simple Library Application
+
+books = ["Science", "History", "Hannibal"]
+
+def show_books():
+    print("\nAvailable Books:")
+    for b in books:
+        print("-", b)
+
+def add_book(book):
+    books.append(book)
+    print(book, "added successfully!")
+
+def remove_book(book):
+    if book in books:
+        books.remove(book)
+        print(book, "removed successfully!")
+    else:
+        print("Book not found!")
+
+# Demo
+show_books()
+add_book("Prison Break")
+remove_book("Escape from Alcatraz")
+show_books()
